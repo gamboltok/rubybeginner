a482016fcc6b2686840da29c170fcc2a2cf43e97
Mikasa <rootssl@mail.ru>
2019-03-12 14:38:59 +0200

    File.delete("file")

--- filev2.rb
+++ filev2.rb
@@ -10,5 +10,17 @@ f1.close
 
 
 # read file
-
 f1 = File.open("filev2.txt", "r"){|file|  puts file.read} # r - чтение файла
+
+
+puts "женлаете удалить файл filev2.txt? yes/no?"
+question = gets.chomp
+case question
+when "yes"
+  then  File.delete("filev2.txt")
+    puts "файл удалён"
+when "no"
+    then puts "файл не был удалён"
+else
+    puts "ошибька ввода"
+end
