2202b85ffa67394f31c096aa83a5bff2b6776390
Mikasa <rootssl@mail.ru>
2019-03-14 17:21:54 +0200

    Update threads.rb

--- threads.rb
+++ threads.rb
@@ -3,7 +3,7 @@ th1 = Thread.new{
     count = 0
     10.times{
         count +=1
-        puts "Thread №1: iteration #{count}"
+        puts "Thread №1: iteration #{count} STATUS: #{th1.status}"
     }
 }
 
@@ -12,9 +12,11 @@ th2 = Thread.new{
     count = 0
     10.times{
         count +=1
-        puts "Thread №2: iteration #{count}"
+        puts "Thread №2: iteration #{count} STATUS: #{th2.status}"
     }
 }
 
+
 th1.join
 th2.join
+puts "\n\nTH1 STATUS: #{th1.status}, TH2 STATUS: #{th2.status}"
