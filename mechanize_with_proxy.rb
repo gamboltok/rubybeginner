a7ba866e7cc4c3a67db1c4406e9e1b39555e1575
Mikasa <rootssl@mail.ru>
2019-03-15 12:39:09 +0200

    Update mechanize_with_proxy.rb

--- mechanize_with_proxy.rb
+++ mechanize_with_proxy.rb
@@ -3,8 +3,10 @@ require 'mechanize'
 agent = Mechanize.new
 agent.user_agent_alias = "Mac Safari"
 agent.set_proxy("46.41.148.152", 3128)
-page = agent.get "https://iplogger.org/2tRJk5"
+agent.keep_alive
+page = agent.get "https://www.youtube.com/watch?v=TBmvwMdA4Uc"
 page
 puts "Enter => Close program"
 gets.chomp
+
 
